e926bfd19a8ae0ab89e184c6dd3f59774acf183a
Max Dignan <maxdignan@gmail.com>
2019-09-23 14:55:28 -0400

    ready for heroku attempt

---
+++ django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/maxdignan/Documents/School/autumn19/capstoneAI/NovaBackend/django_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
