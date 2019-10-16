a3dde1d8bb8d119a436d3c7fd92cf7374bacdad0
Tahir Saif <tahirsaif100@gmail.com>
2019-10-15 20:10:20 -0400

    adding the nltkServices to branch

--- nova/nltkServices.py
+++ nova/nltkServices.py
@@ -1,35 +1,7 @@
-# import requests
-# graph_api_version = 'v2.9'
-# # paste your access token below
-# access_token = ' '
-# # LHL's Facebook user id
-# user_id = '125845680811480'
-# # the id of LHL's response post at https://www.facebook.com/leehsienloong/posts/1505690826160285
-# post_id = '1505690826160285'
-# # the graph API endpoint for comments on LHL's post
-# url = 'https://graph.facebook.com/{}/{}_{}/comments'.format(graph_api_version, user_id, post_id)
-# comments = []
-# r = requests.get(url, params={'access_token': access_token})
-# while True:
-#     data = r.json()
-# # catch errors returned by the Graph API
-#     if 'error' in data:
-#         raise Exception(data['error']['message'])
-# # append the text of each comment into the comments list
-#     for comment in data['data']:
-#         # remove line breaks in each comment
-#         text = comment['message'].replace('\n', ' ')
-#         comments.append(text)
-# print('got {} comments'.format(len(data['data'])))
-# # check if there are more comments
-#     if 'paging' in data and 'next' in data['paging']:
-#         r = requests.get(data['paging']['next'])
-#     else:
-#         break
-# # save the comments to a file
-# with open('comments.txt', 'w', encoding='utf-8') as f:
-#     for comment in comments:
-#         f.write(comment + '\n')
+import requests
+url='http://127.0.0.1:8000/nova/conversations/1'
+r = requests.get(url)
+print(r)
 
 import nltk # be sure to have stopwords installed for this using nltk.download_shell()
 import pandas as pd
